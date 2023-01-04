1d2b71d34aa70bb09ce2c1227e2b65c6d4f3a6fd
HalfSeraphic <VIradVT@gmail.com>
2023-01-03 22:31:01 -0600

    Seguimos

--- 07-string-methods.php
+++ 07-string-methods.php
@@ -1,4 +1,25 @@
 <?php include 'includes/header.php';
+$nombreCliente = "Juan Pablo";
+
+echo strlen($nombreCliente); //Permite conocer extension y toma en cuenta espacios en blanco
+var_dump($nombreCliente); //Similar arriba, es para validar formularios
+
+//Eliminar espacios en blanco
+$texto = trim($nombreCliente); //Corta espacios en blanco
+echo strlen($texto);
+
+//Convertirlo en mayusculas sin CSS
+echo strtoupper($nombreCliente);
+
+//Convertirlo en minusculas
+echo strtolower($nombreCliente);
+
+$mail1 = "correo@correo.com";
+$mail2 = "Correo@correo.com";
+
+var_dump($mail1 === $mail2); //Comprobacion que dara falso debido a la mayuscula
+var_dump(strtolower($mail1) === strtolower($mail2)); //Aqui dara true debido a que normaliza ambos a mayusculas
+
 
 
 
