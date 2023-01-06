228b358e3695d400ddaee31460a193bcd92389ca
HalfSeraphic <VIradVT@gmail.com>
2023-01-05 23:28:56 -0600

    Continuando curso

--- 12-condicionales.php
+++ 12-condicionales.php
@@ -1,6 +1,82 @@
 <?php include 'includes/header.php';
 
+$autenticado = false;
+$admin = false;
 
+if($autenticado) {
+    echo "Usuario autenticado correctamente";
+} else {
+    echo "Usuario no autenticado, inicia sesion";
+}
+
+
+if($autenticado|| $admin) { //Verifica que al menos una condicion se cumpla
+    echo "Usuario autenticado correctamente";
+} else {
+    echo "Usuario no autenticado, inicia sesion";
+}
+
+if($autenticado && $admin) { //Verifica que varias condiciones se cumplan
+    echo "Usuario autenticado correctamente";
+} else {
+    echo "Usuario no autenticado, inicia sesion";
+}
+
+//If anidados... (hay dos if, uno dentro de otro)
+$cliente = [
+    'nombre' => 'Juan',
+    'saldo' => 200,
+    'informacion' => [
+        'tipo' => 'premium'
+    ],
+];
+
+echo "<br>";
+
+if(!empty($cliente)) { //Si hay un signo de exclamacion antes revisa lo contrario
+    echo "El arreglo de cliente no esta vacio";
+
+    if ($cliente['saldo'] > 0) {
+        echo "El saldo del cliente esta disponible";
+    } else {
+        echo "No hay saldo";
+    }
+    
+} else {
+    echo "El arreglo de cliente esta vacio";
+}
+
+
+echo "<br>";
+if ($cliente['saldo'] > 0) {
+    echo "El cliente tiene saldo";
+} else if ($cliente['informacion']['tipo'] === 'premium'){ //Esto da una segunda comprobacion para cumplir esta si la primera no se pudo cumplir
+    echo 'El cliente es premium';
+} else {
+    echo 'No hay cliente definido o no tiene saldo';
+}
+
+//Switch
+echo "<br>";
+$tecnologia = 'JavaScript';
+
+switch ($tecnologia) {
+    case 'PHP':
+        echo 'php, un excelente lenguaje';
+        break;
+
+    case 'JavaScript':
+        echo 'Lenguiaje hecho para la web';
+        break;
+    case 'HTML':
+        echo 'No se considera lenguaje de programacion';
+        break;    
+
+
+    default:
+        echo 'Algun lenguaje que no se cual es';
+        break;
+}
 
 
 
--- 13-loops.php
+++ 13-loops.php
@@ -1,6 +1,58 @@
 <?php include 'includes/header.php';
 
+//While
+$i = 0; //Inicializador
 
+while ($i < 10) {
+    echo $i . "<br>";
+
+    $i++; //Incremento
+}
+
+
+//Do While se ejecuta hasta que la condicion se evalue como falsa
+
+$i = 0; //Inicializador
+do {
+    echo $i . "<br>";
+
+    $i++; //Incremento
+} while ($i < 10);
+
+//For Loop
+for ($i=0; $i < 10; $i++) { 
+    echo $i . "<br>";
+}
+
+
+/*Imprimir 
+3 Fizz
+5 Buzz
+3 y 5 FizzBuzz
+*/
+
+for ($i=0; $i < 100; $i++) { 
+    if ($i % 15 === 0 && $i % 5 === 0) {
+        echo $i . " - Fizz Buzz <br/>";
+    } else if($i % 3 === 0) {
+        echo $i . " - Fizz <br/>";
+    } else if ($i % 5 === 0 ) {
+        echo $i . " - Buzz <br/>";
+    }
+}
+
+//For Each Loop
+$clientes = array('Pedro', 'Juan', 'Karen');
+
+foreach ($clientes as $cliente) {
+    echo $cliente . "<br>";
+}
+
+echo count($clientes);
+
+for ($i=0; $i < count($clientes) ; $i++) { 
+    echo $clientes[$i] . "<br>";
+}
 
 
 include 'includes/footer.php';
